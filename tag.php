91f849755090d721bb7e93821a03d3a0b7b2bb1a
northbeachmedia <ben.degroot@northbeach.media>
2020-08-25 20:29:51 -0400

    CSS update

--- tag.php
+++ tag.php
@@ -86,28 +86,63 @@
 
   <div class="posts-page-top">
 
-    <div class="row no-gutters">
+    <div class="content-loop">
 
-      <h2><?php single_tag_title(); ?></h2>
+        <div class="grid" data-masonry='{ "itemSelector": ".grid-item" }'>
 
-      <div class="col-xs-12">
+          <?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>
 
-        <?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>
+            <?php if (in_category( 'journal' )) : ?>
 
-          <a href="<?php the_permalink(); ?>"><p><?php the_title(); ?></p></a>
+              <div class="grid-item">
+                <a href="<?php the_permalink(); ?>">
+                  <div class="journal-category">
+                    <div class="card">
+                      <div class="card-image">
+                        <?php the_post_thumbnail(); ?>
+                      </div>
+                      <div class="card-body">
+                        <p class="title"><?php the_title(); ?></p>
+                        <p class="date"><?php echo human_time_diff( get_the_time('U'), current_time('timestamp') ) . ' ago'; ?></p>
+                        <p><?php the_excerpt(); ?></p>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
 
-          <div class="excerpt">
-            <a href="<?php the_permalink(); ?>"><p><?php the_excerpt(); ?></p></a>
-          </div>
+            <?php else : ?>
 
-        <?php endwhile; else: ?>
+              <div class="grid-item">
+                <a href="<?php the_permalink(); ?>">
+                  <div class="insight-category">
+                    <div class="card">
+                      <div class="insight-label">
+                        <p>Insight</p>
+                      </div>
+                      <div class="card-body">
+                        <p class="title"><?php the_title(); ?></p>
+                        <p class="date"><?php echo human_time_diff( get_the_time('U'), current_time('timestamp') ) . ' ago'; ?></p>
+                        <p><?php the_excerpt(); ?></p>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
 
-        <?php endif; ?>
+              <?php endif; ?>
 
-      </div>
-    </div>
+          <?php endwhile; else: ?>
 
-  </div>
+          <?php endif; ?>
+
+        </div> <!-- grid -->
+
+      <?php next_posts_link( '<div class="button-see-more">See more</div>' ); ?>
+
+    </div> <!-- .content-loop -->
+
+  </div> <!-- .posts-page-bottom -->
 
   <div class="posts-page-bottom">
   </div>
