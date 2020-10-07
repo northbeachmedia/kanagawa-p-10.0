bc03e0c655ea1f45579657362dadf83ce15ffa14
northbeachmedia <ben.degroot@northbeach.media>
2020-10-06 21:13:37 -0400

    Footer and Inspiration Example component update.

--- insight-single.php
+++ insight-single.php
@@ -184,7 +184,10 @@ template post type: post
 
                 <ul class="nav navbar-nav">
                   <li class="nav-item">
-                    <a class="nav-link" href="<?php echo get_option('home'); ?>/instagram">insta</a>
+                    <a class="nav-link" href="https://open.spotify.com/user/v947bxvsytdi3v5510lhcqtqa">spotify</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?php echo get_option('home'); ?>/instagram">instagram</a>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link" href="<?php echo get_option('home'); ?>/privacy-policy">privacy policy</a>
