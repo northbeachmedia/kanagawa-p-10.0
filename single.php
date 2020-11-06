c35c972d76f30767bb534bdf45758b6acdcfbee7
northbeachmedia <ben.degroot@northbeach.media>
2020-11-06 17:14:00 -0500

    Mobile Updates

--- single.php
+++ single.php
@@ -66,47 +66,7 @@
 
 </div> <!-- single-page -->
 
-<footer>
-
-  <div class="footer-top">
-  </div>
-
-  <div class="footer-bottom">
-
-    <div class="footer-navigation-outer">
-      <div class="footer-navigation-inner">
-
-        <nav class="navbar navbar-light">
-          <div class="navbar-inner">
-            <div class="row">
-              <div class="col-xs-12">
-
-                <ul class="nav navbar-nav">
-                  <li class="nav-item">
-                    <a class="nav-link" href="https://open.spotify.com/user/v947bxvsytdi3v5510lhcqtqa">spotify</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="<?php echo get_option('home'); ?>/instagram">insta/a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="<?php echo get_option('home'); ?>/privacy-policy">privacy</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="<?php echo get_option('home'); ?>/terms">terms</a>
-                  </li>
-                </ul>
-
-              </div>
-            </div>
-          </div>
-        </nav>
-
-      </div>
-    </div>
-
-  </div> <!-- footer-bottom -->
-
-</footer>
+<?php get_template_part('footer-secondary') ?>
 
 <?php wp_footer(); ?>
 
