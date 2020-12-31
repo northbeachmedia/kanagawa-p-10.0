7533d962513470a14529f96fe331f6f1b6ea9e63
northbeachmedia <ben.degroot@northbeach.media>
2020-12-31 00:13:08 -0500

    31/12/2020

---
+++ sidebar-secondary.php
@@ -0,0 +1,31 @@
+<div id="sidebar">
+
+  <div id="dismiss">
+    <i class="fas fa-arrow-right"></i>
+  </div>
+
+  <div class="sidebar-content">
+
+    <ul>
+
+      <li class="list-group-item">
+        <a class="nav-link" href="<?php echo get_option('home'); ?>/#about-page">about</a>
+      </li>
+
+      <li class="list-group-item">
+        <a class="nav-link" href="<?php echo get_option('home'); ?>/#words-page">words</a>
+      </li>
+
+      <li class="list-group-item">
+        <a class="nav-link" href="<?php echo get_option('home'); ?>/#work-page">work</a>
+      </li>
+
+      <li class="list-group-item">
+        <a class="nav-link" href="<?php echo get_option('home'); ?>/#contact">contact</a>
+      </li>
+
+    </ul>
+
+  </div> <!-- sidebar-content -->
+
+</div> <!-- sidebar -->
