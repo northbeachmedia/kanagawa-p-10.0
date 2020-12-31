8dcfe59407e09b49be4402ae74f5248c8526162f
northbeachmedia <ben.degroot@northbeach.media>
2020-12-30 23:49:05 -0500

    Footer update 30/12/2020

--- footer-secondary.php
+++ footer-secondary.php
@@ -6,23 +6,20 @@
 
       <div class="flex-container">
         <div class="flex-item-1">
-          <h4>Making the Internet simple and beautiful one line of code at a time</h4>
-        </div>
-        <div class="flex-item-2">
           <ul>
             <li><a href="<?php echo get_option('home'); ?>/#home-page">home</a></li>
             <li><a href="<?php echo get_option('home'); ?>/#about-page">about</a></li>
             <li><a href="<?php echo get_option('home'); ?>/#words-page">words</a></li>
           </ul>
         </div>
-        <div class="flex-item-3">
+        <div class="flex-item-2">
           <ul>
             <li><a href="<?php echo get_option('home'); ?>/#work-page">work</a></li>
             <li><a href="<?php echo get_option('home'); ?>/#contact">contact</a></li>
             <li><a target="_blank" href="https://open.spotify.com/user/v947bxvsytdi3v5510lhcqtqa">spotify</a></li>
           </ul>
         </div>
-        <div class="flex-item-4">
+        <div class="flex-item-3">
           <ul>
             <li><a target="_blank" href="https://www.instagram.com/degrootcanada/">instagram</a></li>
             <li><a href="<?php echo get_option('home'); ?>/privacy-policy">privacy</a></li>
--- footer.php
+++ footer.php
@@ -31,23 +31,20 @@
 
       <div class="flex-container">
         <div class="flex-item-1">
-          <h4>Making the Internet simple and beautiful one line of code at a time</h4>
-        </div>
-        <div class="flex-item-2">
           <ul>
             <li><a href="#home-page">home</a></li>
             <li><a href="#about-page">about</a></li>
             <li><a href="#words-page">words</a></li>
           </ul>
         </div>
-        <div class="flex-item-3">
+        <div class="flex-item-2">
           <ul>
             <li><a href="#work-page">work</a></li>
             <li><a href="#contact">contact</a></li>
             <li><a target="_blank" href="https://open.spotify.com/user/v947bxvsytdi3v5510lhcqtqa">spotify</a></li>
           </ul>
         </div>
-        <div class="flex-item-4">
+        <div class="flex-item-3">
           <ul>
             <li><a target="_blank" href="https://www.instagram.com/degrootcanada/">instagram</a></li>
             <li><a href="<?php echo get_option('home'); ?>/privacy-policy">privacy</a></li>
